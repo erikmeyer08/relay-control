36f23c3b083a1b028b96cc5dd634f8cbde3bd755
erikmeyer08 <43043270+erikmeyer08@users.noreply.github.com>
2026-09-25 16:15:20 -0400

    fix: model onoff safe output initialization

--- src/types/onoff.d.ts
+++ src/types/onoff.d.ts
@@ -1,6 +1,15 @@
 declare module 'onoff' {
   export class Gpio {
-    constructor(gpio: number, direction: 'in' | 'out');
+    constructor(
+      gpio: number,
+      direction: 'in' | 'out' | 'high' | 'low',
+      edge?: 'none' | 'rising' | 'falling' | 'both',
+      options?: {
+        activeLow?: boolean;
+        reconfigureDirection?: boolean;
+        debounceTimeout?: number;
+      }
+    );
     readSync(): 0 | 1;
     writeSync(value: 0 | 1): void;
     unexport(): void;
